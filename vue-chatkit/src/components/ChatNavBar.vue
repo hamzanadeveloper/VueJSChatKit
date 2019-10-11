ae195717d9e648df2f924fa6666ccbb27989f2b4
hamzanadeveloper <hamza.arshad.developer@gmail.com>
2019-10-11 00:42:47 -0400

    Styling changes

--- vue-chatkit/src/components/ChatNavBar.vue
+++ vue-chatkit/src/components/ChatNavBar.vue
@@ -1,11 +1,9 @@
 <template>
     <b-navbar id="chat-navbar" toggleable="md" type="dark" variant="info">
-        <b-navbar-brand href="#">
-            Vue Chat
-        </b-navbar-brand>
+        <b-navbar-brand class="vue-title" href="#">VUECHAT</b-navbar-brand>
         <b-navbar-nav class="ml-auto">
-            <b-nav-text>{{ user.name }} | </b-nav-text>
-            <b-nav-item href="#" @click="onLogout" active>Logout</b-nav-item>
+            <b-nav-text class="nav-text">{{ user.name }} | </b-nav-text>
+            <b-nav-item class="nav-text" href="#" @click="onLogout" active>Logout</b-nav-item>
         </b-navbar-nav>
     </b-navbar>
 </template>
@@ -49,7 +47,21 @@
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css?family=Fjalla+One|Montserrat&display=swap');
+
     #chat-navbar {
         margin-bottom: 15px;
+        background-color: rgb(46,49,53) !important;
+        -webkit-box-shadow:0px 1px 1px #060106;
+        -moz-box-shadow:0px 1px 1px #060106;
+        box-shadow:0px 1px 1px #060106;
+    }
+
+    .vue-title{
+        font-family: 'Fjalla One', sans-serif;
+    }
+
+    .nav-text{
+        font-family: 'Montserrat', sans-serif;
     }
 </style>
